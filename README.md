0c8e52c95ed6356bd1467ababbc80fd3094e095a
hmmini <pak4184@naver.com>
2023-03-18 18:39:52 +0900

    basic setting - Application.yml, build.gradle, QueryDsl

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # querydsl-study
-querydsl + data jpa
+- 실전에서 사용하는 QueryDsl을 공부
+- 동적 쿼리 및 복잡한 쿼리에서의 사용법을 공부
--- src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,42 @@
 package study.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Hello;
+import study.querydsl.entity.QHello;
+
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
+@Transactional
+@Rollback(value = false)
 class QuerydslApplicationTests {
 
+	@Autowired EntityManager em;
+
 	@Test
+	@DisplayName("queryDsl Q class test")
 	void contextLoads() {
+		//given
+		Hello hello = new Hello();
+		em.persist(hello);
+
+		JPAQueryFactory query = new JPAQueryFactory(em);
+//		QHello qHello = new QHello("h"); //String : alias를 넣어줌
+		QHello qHello = QHello.hello;
+		Hello result = query
+				.selectFrom(qHello)
+				.fetchOne();
+
+		assertThat(result).isEqualTo(hello);
+		assertThat(result.getId()).isEqualTo(hello.getId());
 	}
 
 }
