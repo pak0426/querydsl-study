9bb3a87484fce489662e2e4a4dad57f371af4b27
hmmini <pak4184@naver.com>
2023-03-29 22:25:35 +0900

    동적 쿼리와 성능 최적화 조회 - where절 파라미터 사용

--- src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
@@ -53,6 +53,35 @@ class MemberJpaRepositoryTest {
     }
 
     @Test
+    public void searchTestBuilder() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        MemberSearchCondition condition = new MemberSearchCondition();
+        condition.setAgeGoe(35);
+        condition.setAgeLoe(40);
+        condition.setTeamName("teamB");
+
+        List<MemberTeamDto> result = memberJpaRepository.searchByBuilder(condition);
+
+        for (MemberTeamDto memberTeamDto : result) {
+            System.out.println("memberTeamDto = " + memberTeamDto);
+        }
+
+        assertThat(result).extracting("username").containsExactly("member3", "member4");
+    }
+
+    @Test
     public void searchTest() {
         Team teamA = new Team("teamA");
         Team teamB = new Team("teamB");
@@ -67,17 +96,13 @@ class MemberJpaRepositoryTest {
         em.persist(member3);
         em.persist(member4);
 
-        MemberSearchCondition searchCondition = new MemberSearchCondition();
-//        searchCondition.setAgeGoe(35);
-//        searchCondition.setAgeLoe(40);
-        searchCondition.setTeamName("teamB");
+        MemberSearchCondition condition = new MemberSearchCondition();
+        condition.setAgeGoe(35);
+        condition.setAgeLoe(40);
+        condition.setTeamName("teamB");
 
-        List<MemberTeamDto> result = memberJpaRepository.searchByBuilder(searchCondition);
+        List<MemberTeamDto> result = memberJpaRepository.search(condition);
 
-        for (MemberTeamDto memberTeamDto : result) {
-            System.out.println("memberTeamDto = " + memberTeamDto);
-        }
-
-        assertThat(result).extracting("username").containsExactly("member3", "member4");
+        assertThat(result).extracting("username").containsExactly("member4");
     }
 }
