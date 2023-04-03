76308a8d5077396e5b1b808de0a6d077385fb476
hmmini <pak4184@naver.com>
2023-04-04 00:20:02 +0900

    인터페이스 지원 - QueryPredicateExcutor

--- src/test/java/study/querydsl/repository/MemberRepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
 import study.querydsl.entity.Team;
 
 import javax.persistence.EntityManager;
@@ -112,5 +113,28 @@ class MemberRepositoryTest {
 
         assertThat(result.getSize()).isEqualTo(2);
         assertThat(result.getContent()).extracting("username").containsExactly("member3", "member4");
+    }
+
+    @Test
+    public void queryDslPredicateExecutorTest() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        QMember member = QMember.member;
+        Iterable<Member> result = memberRepository.findAll(member.age.between(20, 40).and(member.username.eq("member3")));
+
+        for (Member row : result) {
+            System.out.println("row = " + row);
+        }
     }
 }
